a2ccc259f38ae6ace18dfcad4dd20ea7801ae263
Victor Matzar <elmatzar123@gmail.com>
2023-04-14 18:16:01 -0600

    courses endpoints modified

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -30,9 +30,9 @@ const getStudentById = async (req, res) => {
 
 const updateStudent = async (req, res) => {
     const id = req.params.id;
-    const { name,date_of_birth, gender } = req.body;
+    const { name, date_of_birth, gender } = req.body;
     await pool.query('UPDATE STUDENTS SET NAME = $1, DATE_OF_BIRTH = $2, GENDER = $3 WHERE STUDENT_ID = $4', [
-        name,date_of_birth, gender, id
+        name, date_of_birth, gender, id
     ], (error, results) => {
         if (error) throw error;
         console.log(results);
@@ -48,10 +48,10 @@ const deleteStudent = async (req, res) => {
 };
 
 const postStudent = async (req, res) => {
-    const { student_id, name,date_of_birth, gender, email, password } = req.body;
+    const { student_id, name, date_of_birth, gender, email, password } = req.body;
     await pool.query('INSERT INTO STUDENTS (STUDENT_ID, NAME, DATE_OF_BIRTH, GENDER, EMAIL, PASSWORD) VALUES ($1, $2, $3, $4, $5, $6)',
         [
-            student_id, name,date_of_birth, gender, email, password
+            student_id, name, date_of_birth, gender, email, password
         ],
         (error, results) => {
             if (error) throw error;
@@ -63,8 +63,14 @@ const postStudent = async (req, res) => {
             });
         });
 }
-
 const getCourses = async (req, res) => {
+    await pool.query(
+        'SELECT * FROM COURSES', (error, results) => {
+            if (error) throw error;
+            return res.status(200).json(results.rows);
+        });
+};
+const getStudentCourses = async (req, res) => {
     const id = req.params.id;
     await pool.query(
         'SELECT c.*, CASE WHEN cs.student_id IS NOT NULL THEN true ELSE false END AS assigned FROM courses c LEFT JOIN registration cs ON c.course_id = cs.course_id AND cs.student_id = $1', [id], (error, results) => {
@@ -82,9 +88,9 @@ const getCourseById = async (req, res) => {
 
 const updateCourse = async (req, res) => {
     const id = req.params.id;
-    const { name,credits,teacher } = req.body;
+    const { name, credits, teacher } = req.body;
     await pool.query('UPDATE COURSES SET NAME = $1, CREDITS = $2, TEACHER = $3 WHERE COURSE_ID = $4', [
-        name,credits,teacher
+        name, credits, teacher, id
     ], (error, results) => {
         if (error) throw error;
         console.log(results);
@@ -94,16 +100,21 @@ const updateCourse = async (req, res) => {
 const deleteCourse = async (req, res) => {
     const id = req.params.id;
     await pool.query('DELETE FROM COURSES WHERE COURSE_ID = $1', [id], (error, results) => {
-        if (error) throw error;
-        res.json(`Course data with id ${id} deleted succesfully`);
+        if (error) {
+            res.status(500).json({
+                message: "Existen estudiante(s) asignados a este curso, primero deben desasignarse para poder eliminar este curso"
+            });
+        } else {
+            res.json(`Course data with id ${id} deleted succesfully`);
+        }
     });
 };
 
 const postCourse = async (req, res) => {
-    const { name,credits,teacher } = req.body;
+    const { name, credits, teacher } = req.body;
     await pool.query('INSERT INTO COURSES (NAME, CREDITS, TEACHER) VALUES ($1, $2, $3)',
         [
-            name,credits,teacher
+            name, credits, teacher
         ],
         (error, results) => {
             if (error) throw error;
@@ -119,13 +130,13 @@ const postCourse = async (req, res) => {
 
 const deleteRegistration = async (req, res) => {
     const { student_id, course_id } = req.body;
-    await pool.query('DELETE FROM REGISTRATION WHERE STUDENT_ID = $1 AND COURSE_ID = $2',        
-    [
-        student_id, course_id
-    ], (error, results) => {
-        if (error) throw error;
-        res.json(`registration data with id ${id} deleted succesfully`);
-    });
+    await pool.query('DELETE FROM REGISTRATION WHERE STUDENT_ID = $1 AND COURSE_ID = $2',
+        [
+            student_id, course_id
+        ], (error, results) => {
+            if (error) throw error;
+            res.json(`registration data deleted succesfully`);
+        });
 };
 
 const postRegistration = async (req, res) => {
@@ -137,14 +148,15 @@ const postRegistration = async (req, res) => {
         (error, results) => {
             if (error) throw error;
             res.status(201).json({
-                message: "Student data added Succesfully",
+                message: "Registration data added Succesfully",
                 body: {
-                    student: req.body
+                    registration: req.body
                 }
             });
         });
 }
 module.exports = {
     getStudents, getStudentById, updateStudent, deleteStudent, postStudent,
-    getCourses, getCourseById, updateCourse, deleteCourse, postCourse
+    getCourses, getStudentCourses, getCourseById, updateCourse, deleteCourse, postCourse,
+    deleteRegistration, postRegistration
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const cors = require('cors');
 const { getStudents, getStudentById, updateStudent, deleteStudent, postStudent,
-    getCourses, getCourseById, updateCourse, deleteCourse, postCourse } = require('../controllers/index.controllers');
+    getCourses, getStudentCourses, getCourseById, updateCourse, deleteCourse, postCourse, postRegistration, deleteRegistration } = require('../controllers/index.controllers');
 
 //Students
 router.get('/students', cors(), getStudents);
@@ -12,10 +12,14 @@ router.post('/students', cors(), postStudent);
 router.delete('/students/:id', cors(), deleteStudent);
 
 //Courses
-router.get('/courses/:id', cors(), getCourses);
-// router.get('/courses/:id', cors(), getCourseById);
+router.get('/courses', cors(), getCourses);
+router.get('/courses/:id', cors(), getStudentCourses);
+router.get('/course/:id', cors(), getCourseById);
 router.put('/courses/:id', cors(), updateCourse);
 router.post('/courses', cors(), postCourse);
 router.delete('/courses/:id', cors(), deleteCourse);
 
+//registration
+router.post('/registration', cors(), postRegistration);
+router.delete('/registration', cors(), deleteRegistration);
 module.exports = router;
